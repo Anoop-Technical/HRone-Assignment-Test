dc64ec8e07e8c26952971cb7a78a44ffd2f16824
anoop pandey <anoopandey15@gmail.com>
2023-03-20 22:11:19 +0530

    Assignment Test Uploaded

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $(".seeall_Leave").click(function () {
+        $(".cu_toggle1").toggle("slow");
+    });
+    $(".seeall_initiative").click(function () {
+        $(".cu_toggle2").toggle("slow");
+    });
+    $(".seeDetails").click(function () {
+        $(".chair").toggle("slow");
+    });
+    $(".seeDetails2").click(function () {
+        $(".yesterday").toggle("slow");
+    });
+    $(".c_more1").click(function () {
+        $(".cmo1").toggle("slow");
+    });
+    $(".c_more2").click(function () {
+        $(".cmo2").toggle("slow");
+    });
+    $(".c_more3").click(function () {
+        $(".cmo3").toggle("slow");
+    });
+    $(".c_more4").click(function () {
+        $(".cmo4").toggle("slow");
+    });
+});
